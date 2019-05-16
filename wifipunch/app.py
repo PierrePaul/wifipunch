674c5003617e57b7658ee9bc19cd70c10141be16
Aurélien Bondis <aurelien.bondis@gmail.com>
2019-05-16 14:21:29 -0400

    add db stuff, fix dependencies

--- wifipunch/app.py
+++ wifipunch/app.py
@@ -2,6 +2,8 @@
 from .discover import get_local_ip, scan
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, fields, marshal_with
+
 app = Flask(__name__)
 app.config['DATABASE_URL'] = "postgresql://db/wifipunch"
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://db/wifipunch"
@@ -15,7 +17,6 @@ class MacAddress(db.Model):
     __tablename__ = 'macaddress'
     id = db.Column(db.Integer, primary_key=True)
     mac = db.Column(db.String())
-    # TODO: o2m(timelog)
 
 
 class TimeLog(db.Model):
@@ -28,14 +29,34 @@ class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String())
-    # TODO: m2m(macaddress)
+    # One to Many (macaddress)
+    mac_addresses_id = db.Column(
+        db.Integer,
+        db.ForeignKey('macaddress.id')
+    )
+    mac_addresses = db.relationship(
+        'MacAddress',
+        backref=db.backref('users', lazy=True)
+    )
 
 
+user_fields = {
+    'name': fields.String,
+    'mac_addresses': fields.List(
+        fields.String(attribute='mac'),
+    )
+}
+
+
+@marshal_with(
+    user_fields,
+    envelope='resource'
+)
 @app.route("/user", methods=['GET'])
 def list_users():
     """
     """
-    users = []
+    users = User.query.all()
     return users
 
 
