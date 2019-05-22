a7b78dda0755cab0d45f6e090a479e3de7d1afac
Aurélien Bondis <aurelien.bondis@gmail.com>
2019-05-22 13:06:17 -0400

    fix script to generate json api sample

--- wifipunch/test_api.sh
+++ wifipunch/test_api.sh
@@ -1,25 +1,38 @@
-#!/bin/sh -x
+#!/bin/sh
 host=$1
 if [ "x_$host" = "x_" ]; then
     host="localhost:5000"
 fi
+get="http -b GET $host"
+post="http -b POST $host"
 
+commands=$(echo "
 # Testing User API
-http GET $host/user
-http POST $host/user username=test
-http POST $host/user username=test
-http GET $host/user/test
+$get/user
+$post/user username=test
+$post/user username=test
+$get/user/test
 
 # Testing Link API
-http GET $host/link
-http POST $host/link username=test
-http POST $host/link mac_address=12:23:34:22:65 username=test
-http POST $host/link mac_address=12:23:34:22:65 username=nobody
+$get/link
+$post/link username=test
+$post/link mac_address=12:23:34:22:65 username=test
+$post/link mac_address=12:23:34:22:65 username=nobody
 
 # Testing Mac API
-http GET $host/mac
-http GET $host/mac/mine
+$get/mac
+$get/mac/mine
 
 # Testing Log API
-http POST $host/mac/log
-http GET $host/mac/log
+$post/mac/log
+$get/mac/log
+"|grep -v -e '^#' -e '^ *$')
+
+oIFS=$IFS
+nIFS=$'\n'
+IFS=$nIFS
+for com in $commands; do
+	IFS=$oIFS
+	echo "// $com"
+	$com
+done
