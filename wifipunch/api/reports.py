13c9e44abe1998c2fe363cc7bc115720e3c203bc
Aurélien Bondis <aurelien.bondis@gmail.com>
2019-06-03 13:19:55 -0400

    reporting: partial rewrite to render csv

--- wifipunch/api/reports.py
+++ wifipunch/api/reports.py
@@ -1,6 +1,7 @@
-from flask import jsonify, Blueprint
-from collections import defaultdict
+import csv
 from datetime import timedelta
+from flask import jsonify, Blueprint, current_app
+from io import StringIO
 from ..models import TimeLog, User
 
 time_resolution = 60
@@ -10,28 +11,54 @@ max_delta = timedelta(minutes=15)
 report_blueprint = Blueprint('report', __name__, url_prefix='/report')
 
 
+csv_fields = ["user", "period", "total_time"]
+
+
 @report_blueprint.route("", methods=['GET'])
+def get_report():
+    return jsonify(report())
+
+
+def report_to_csv():
+    rep = report()
+    csv_file = StringIO()
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerows(rep)
+    return csv_file.getvalue()
+
+
 def report(delta='1w'):
+    """
+    Generate a report for each user, grouped by dates
+    If two 'checkins' are too far appart (max_delta), the user is
+    considered as logged out between those two times.
+    If two checkins are of different dates, one will be
+    a checkout, the other is a checkin for the next day
+    """
     users = User.query.all()
-    report = {}
+    report_file = []
     for user in users:
         entries = TimeLog.query.filter(
             TimeLog.user == user.name
             # TODO: filter by delta
         ).order_by(TimeLog.time).all()
-        daily_report = defaultdict(float)
         if entries:
+            (entries)
             prev = entries[0]
+            last = entries[-1]
+            total = 0.0
             for entry in entries:
                 date = entry.time.date()
-                if (
-                        date == prev.time.date()
-                ):
+                date_changed = (date != prev.time.date())
+                if not (date_changed):
                     delta = (entry.time - prev.time)
                     if delta < max_delta:
-                        daily_report[
-                            date.isoformat()
-                        ] += delta.total_seconds()/time_resolution
+                        total += delta.total_seconds()/time_resolution
+                if entry == last or date_changed:
+                    report_file.append([
+                            user.name,
+                            prev.time.date().isoformat(),
+                            total
+                    ])
                 prev = entry
-        report[user.name] = daily_report
-    return jsonify(report)
+    return report_file
