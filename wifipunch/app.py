b061e3ae077966005cfeb300c20c41178df3872b
Edouard Duplessis <eduplessis@gmail.com>
2019-06-03 15:34:20 -0400

    flask CORS

--- wifipunch/app.py
+++ wifipunch/app.py
@@ -1,12 +1,14 @@
 #!/bin/env python3
 from flask import Flask
 from flask_migrate import Migrate
+from flask_cors import CORS
 from .models.extensions import db
 from .api import mac_blueprint, user_blueprint, link_blueprint
 
 
 def create_app(config=None):
     app = Flask(__name__)
+    CORS(app)
     app.config[
         'DATABASE_URL'
     ] = "postgresql://wifipunch@db/wifipunch"
